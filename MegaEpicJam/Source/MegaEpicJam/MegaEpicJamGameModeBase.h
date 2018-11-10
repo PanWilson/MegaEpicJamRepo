cad8c6a9a78a44e038676a04e409db0adcbd9c10
PanWilson <PanWilsonn@wp.pl>
2018-11-10 13:54:05 +0100

    init

---
+++ MegaEpicJam/Source/MegaEpicJam/MegaEpicJamGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MegaEpicJamGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEGAEPICJAM_API AMegaEpicJamGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
